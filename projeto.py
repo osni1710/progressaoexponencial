271597e3efaf9cf964fa1d7904e8b1790f977296
osni ribeiro de oliveira <60950179+osni1710@users.noreply.github.com>
2020-03-03 22:25:53 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,11 @@
+#Autores 1DSI3
+#	@Osni
+#Feito em 03/03/2020
+#Instituição: ETEC
+
+x = 0 #Declaração de conteudo
+valor = 2 #Declaração da variavel valor
+while (x <= 21):
+		valor = valor * valor
+		x = x + 1 #Laço de repetição
+print (valor) #Exibir a variavel valor
